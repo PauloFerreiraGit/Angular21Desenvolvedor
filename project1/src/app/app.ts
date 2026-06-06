b1f64caef8b58f4e50370aa3306ddd981c992b89
PauloFerreiraGit <pauloferreiraworkflow@gmail.com>
2026-06-06 22:43:35 +0100

    criação de componentes

---
+++ project1/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { Avatar } from "./components/avatar/avatar";
+import { Header } from "./components/header/header";
+import { Main } from "./components/main/main";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Footer, Avatar, Header, Main],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('project1');
+}
